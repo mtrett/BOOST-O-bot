9e54ddb3b06e932247f7622bea1415a734b6783a
Brad Jones <bradster.jones@gmail.com>
2017-06-12 22:29:41 -0500

    Boost-o doesn't like being cursed out.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var Overcraft = require('./overcraft.js');
 var connectionConfig = require('./_connectionConfig.json');
 var Responder = require("./components/responder.js");
 
+var AngryResponses = require("./strings/angry.json");
+
 const Events = Discordie.Events;
 const client = new Discordie();
 
@@ -16,6 +18,34 @@ const overcraft = new Overcraft(client);
 
 //General listeners
 var generalListener = new Responder(client);
+
 generalListener.addListener("say hi boost-o", function(e) {
     e.message.channel.sendMessage("Hi BOOST-O.");
+});
+
+generalListener.addListener("fuck you", function(e) {
+    var insulted = false;
+    var message = e.message.content.toLowerCase();
+    if (e.message.mentions) {
+        e.message.mentions.forEach(mention => {
+            if(mention.id === client.User.id) {
+                insulted = true;
+            }
+        });
+    }
+
+    if (message.indexOf('robot') >= 0 ||
+        message.indexOf('boost-o') >= 0 ||
+        message.indexOf('omnic') >= 0) {
+        insulted = true;
+    }
+
+    if (!insulted) {
+        return;
+    }
+
+    var insultsLength = AngryResponses.insults.length;
+    var insultIndex = Math.floor(Math.random() * insultsLength);
+    e.message.channel.sendMessage(AngryResponses.insults[insultIndex]);
+    
 });
